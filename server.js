3322ccabb401226015496e2502493ad57834062b
Asim Aftab <asim.dev.cs@gmail.com>
2024-12-27 19:26:24 +0530

    Login Bug Fixed

--- server.js
+++ server.js
@@ -86,10 +86,10 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: 'Something went wrong!' });
 });
 
-// // Route not found handler
-// app.use((req, res, next) => {
-//     res.status(404).json({ error: 'Route not found' });
-// });
+// Route not found handler
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Route not found' });
+});
 
 // Start the server
 app.listen(process.env.PORT || 5000, () => {
