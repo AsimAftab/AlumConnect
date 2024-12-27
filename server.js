cc29612fad69022f95567747947267b8f80fa349
Asim Aftab <asim.dev.cs@gmail.com>
2024-12-27 14:10:09 +0530

    Landing Page Changes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const PORT=process.env.PORT || 5000;
 
@@ -11,9 +12,10 @@ const app=express();
 
 connectDB();
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res)=>{
-    res.send('API Running');
+    res.sendFile(path.join(__dirname,'views','homepage.html'));
 });
 
 app.listen(PORT,()=>{ console.log(`Server started on port ${PORT}`); });
