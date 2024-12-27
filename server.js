292a30d6c25612f3e35907f31bec998c3d666e13
Farhan Ahmed <farhan2004ahmed@gmail.com>
2024-12-28 00:00:07 +0530

    merge changes

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const path = require('path');
-
-const PORT=process.env.PORT || 5000;
-
 const dotenv = require('dotenv');
-const connectDB=require('./config/db');
+const connectDB = require('./config/db');
 
 dotenv.config();
 
-const app=express();
+const PORT = process.env.PORT || 5000;
+const app = express();
 
 connectDB();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views','homepage.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'homepage.html'));
 });
 
-app.listen(PORT,()=>{ console.log(`Server started on port ${PORT}`); });
+app.get('/settings', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'settings.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
