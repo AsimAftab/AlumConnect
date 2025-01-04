6978fc9174d266306ebebab9fa70fa9a3014d835
Asim Aftab <asim.dev.cs@gmail.com>
2025-01-04 23:20:17 +0530

    Integrated Backend for Add new Alumin

--- models/recordModels.js
+++ models/recordModels.js
@@ -6,20 +6,26 @@ const recordSchema = new mongoose.Schema({
     slNo: { type: Number, required: true }, // Serial Number
     name: { type: String, required: true },
     company: { type: String, required: true },
-    usn: { type: String, required: true, unique: true },
+    usn: { type: String, required: true, unique: true,
+        validate: {
+            validator: function (v) {
+                return /^1BY\d{2}[A-Z]{2}\d{3}$/.test(v);
+            },
+           
+        }
+    },
     dateUpdated: {
         type: String, // Store as string to keep only the date part
         default: () => moment().tz("Asia/Kolkata").format("YYYY-MM-DD") // Set default as current date in specific timezone
     },
     batch: { type: Number, required: true },
-    status: { type: String, required: true },
+    status: { type: String, required: false },
     requestUpdate: { type: String, default: "Request Update" }
 }, { collection: 'records', timestamps: true });
 
 // Pre-save middleware to generate Sl.No automatically
 recordSchema.pre("save", async function (next) {
     if (!this.slNo) {
-        // Get the counter collection and increment the value
         const Counter = mongoose.model("Counter", new mongoose.Schema({
             _id: { type: String, required: true },
             seq: { type: Number, default: 0 }
